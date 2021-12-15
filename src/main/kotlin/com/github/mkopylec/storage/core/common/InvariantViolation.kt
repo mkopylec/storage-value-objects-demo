4750cb65302789b2751231507b74bacdde1d7881
mariusz.kopylec <mariusz.kopylec@o2.pl>
2021-12-15 10:11:45 +0100

    Final version deleted from master

--- src/main/kotlin/com/github/mkopylec/storage/core/common/InvariantViolation.kt
+++
@@ -1,6 +0,0 @@
-package com.github.mkopylec.storage.core.common
-
-abstract class InvariantViolation(
-    properties: List<Pair<String, Any?>> = emptyList(),
-    cause: Throwable? = null
-) : RuntimeException(properties.joinToString { "${it.first}=${it.second}" }, cause)
