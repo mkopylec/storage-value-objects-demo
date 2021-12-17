321eace653cb8af2116dcb299b3a7b53118fe1e2
Mariusz Kopylec <mariusz.kopylec@allegro.pl>
2021-12-17 14:00:57 +0100

    Step 5

--- src/main/kotlin/com/github/mkopylec/storage/core/container/ContainerRepository.kt
+++ src/main/kotlin/com/github/mkopylec/storage/core/container/ContainerRepository.kt
@@ -1,7 +1,9 @@
 package com.github.mkopylec.storage.core.container
 
+import com.github.mkopylec.storage.core.container.Container.Identifier
+
 interface ContainerRepository {
 
     fun save(container: Container)
-    fun findByIdentifier(identifier: String): Container?
+    fun findByIdentifier(identifier: Identifier): Container?
 }
